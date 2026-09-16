0cc75cf3cb81a59c04393c113a167d5229291e0f
srkbaskar11 <164205243+srkbaskar11@users.noreply.github.com>
2026-09-16 08:46:58 +0530

    Update outpass_repository.py

--- backend/app/repositories/outpass_repository.py
+++ backend/app/repositories/outpass_repository.py
@@ -9,3 +9,29 @@ class OutpassRepository:
                 {"student_id": student_id}
             ).sort("request_timestamp", -1)
         )
+        def get_by_id(self, outpass_id: str):
+    return outpasses_collection.find_one(
+        {"_id": outpass_id}
+    )
+
+
+def update_status(
+    self,
+    outpass_id: str,
+    status: str,
+    warden_remarks: str = None
+):
+    from datetime import datetime
+
+    result = outpasses_collection.update_one(
+        {"_id": outpass_id},
+        {
+            "$set": {
+                "status": status,
+                "warden_remarks": warden_remarks,
+                "decision_timestamp": datetime.utcnow()
+            }
+        }
+    )
+
+    return result.modified_count > 0
